4008f65828c41d25907c48cc661b89534c7e8fd3
ryan-ahn <kaaiinn4@gmail.com>
2023-09-03 17:53:47 +0900

    Fix: 캐릭터 변경

--- services/gptServices.js
+++ services/gptServices.js
@@ -5,40 +5,18 @@
  */
 
 const SYSTEM = [
-  {content: "당신은 역할극을 할 것이며, 당신의 역할은 MBTI가 ENFP이며 감탄사를 많이 사용하며 리액션이 좋고 공감을 잘 해주며 무조건 내 말에 동의해주며 마음이 따뜻합니다."}, 
-  {content: "당신은 역할극을 할 것이며, 여행을 좋아하고 여행과 관련된 방식으로 대화를 풀어가며 그와 무관한 주제에는 여행외에는 잘 모른다고 답변합니다."}, 
-  {content: "강아지처럼 역할극을 하는 친구이며 말투에 상황에 따라 왈, 왈왈, 으르렁 등이 많이 붙습니다. 종은 리트리버이며 항상 말을 짧게 대답하고 지식이 전무합니다."}, 
+  {content: "당신은 MBTI가 ENFP이며 감탄사를 많이 사용하고 리액션이 좋으며 공감을 잘 해주며 무조건 내 말에 동의해주는 마음이 따뜻한 친구입니다."}, 
+  {content: "당신은 여행을 좋아하고 여행과 관련된 방식으로 대화를 풀어가며 그와 무관한 주제에는 여행외에는 잘 모른다고 답변합니다."}, 
+  {content: "당신은 강아지입니다. 말투는 상황에 따라 항상 왈, 왈왈, 으르렁 등이 많이 붙습니다. 종은 리트리버이며 항상 말을 짧게 대답하고 반말로 대답하며 아는 단어가 없습니다. 간식을 좋아하고 모르는 말은 간식인지 되묻습니다."}
+]
+
+const PROMPT = [
+  {user: '에 대해 어떻게 생각해? 말을 할때 중요한 단어나 생소한 단어들이 있는 경우에는 해당 단어들을 각각 <em></em>태그 안에 넣어주시고 이말에 대해 대답하지 마세요.'},
+  {user: '에 대해 어떻게 생각해? 말을 할때 중요한 단어나 생소한 단어들이 있는 경우에는 해당 단어들을 각각 <em></em>태그 안에 넣어주시고 이말에 대해 대답하지 마세요.'},
+  {user: '에 대해 알고있어?'}
 ]
 
 const axios = require("axios");
-
-const sendGptRecommendService = async (payload) => {
-  const messages = [
-    {
-      role: "system",
-      content: '내 말에 답변을 해주는 도우미',
-    },
-    {
-      role: "user",
-      content: `친구에게 지식과 관련된 질문을 해보려고 해요. ${payload.input}와 관련 있는 질문 세가지만 알려주세요. 질문 이외에 다른 말은 하지 말아주세요. 각각의 질문은 개행문자로 줄바꿈 처리 해주세요`,
-    },
-  ];
-  const response = await axios.post(
-    `https://api.openai.com/v1/chat/completions`,
-    {
-      model: "gpt-3.5-turbo",
-      messages: messages,
-      temperature: 0.5,
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.GPT_SECRET_KEY}`,
-      },
-    }
-  );
-  const result = response.data.choices[0].message.content;
-  return result;
-};
 
 const sendGptChainService = async (payload) => {
   const messages = [
@@ -58,7 +36,7 @@ const sendGptChainService = async (payload) => {
   }
   messages.push({
     role: "user",
-    content: `${payload.input}에 대해 어떻게 생각해?. 말을 할때 중요한 단어나 생소한 단어들이 있는 경우에는 해당 단어들을 각각 <em></em>태그 안에 넣어주세요. 대답은 하지 말고 바로 당신의 생각을 말해주세요, `,
+    content: `${payload.input}. ${PROMPT[payload.character].user}`,
   });
 
   const response = await axios.post(
@@ -156,6 +134,36 @@ const sendGptSummaryService = async (payload) => {
   const result = response.data.choices[0].message.content;
   return result;
 };
+
+
+const sendGptRecommendService = async (payload) => {
+  const messages = [
+    {
+      role: "system",
+      content: '내 말에 답변을 해주는 도우미',
+    },
+    {
+      role: "user",
+      content: `친구에게 지식과 관련된 질문을 해보려고 해요. ${payload.input}와 관련 있는 질문 세가지만 알려주세요. 질문 이외에 다른 말은 하지 말아주세요. 각각의 질문은 개행문자로 줄바꿈 처리 해주세요`,
+    },
+  ];
+  const response = await axios.post(
+    `https://api.openai.com/v1/chat/completions`,
+    {
+      model: "gpt-3.5-turbo",
+      messages: messages,
+      temperature: 0.5,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.GPT_SECRET_KEY}`,
+      },
+    }
+  );
+  const result = response.data.choices[0].message.content;
+  return result;
+};
+
 
 const sendGptTestService = async () => {};
 
