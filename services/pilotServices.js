6c1ec62f9ae4ba9d89cf1b2b0a64523870e0b4ad
ryan-ahn <kaaiinn4@gmail.com>
2023-09-16 19:29:14 +0900

    Modify: 파일럿 수정

--- services/pilotServices.js
+++ services/pilotServices.js
@@ -7,15 +7,19 @@ const sendChatCompletionsService = async (
   prompt,
   temperature,
 ) => {
+  const systemCompletions = [];
+  for (let i = 0; i < system.length; i++) {
+    systemCompletions.push(system[i].text);
+  }
   const messages = [
     {
       role: 'system',
-      content: system,
+      content: systemCompletions.join(),
     },
   ];
   if (assistant && assistant.length > 0) {
     for (let i = 0; i < assistant.length; i++) {
-      messages.push({ role: 'user', content: assistant[i].prompt });
+      messages.push({ role: 'user', content: assistant[i].question });
       messages.push({
         role: 'assistant',
         content: assistant[i].answer,
